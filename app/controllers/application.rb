e62d70e0ce1032ff7cf94bf535baf04e8e1c6871
Tom <user@asus-1710595401.(none)>
2008-05-16 01:21:58 +0100

    Setup clean rails app

---
+++ app/controllers/application.rb
@@ -0,0 +1,10 @@
+# Filters added to this controller apply to all controllers in the application.
+# Likewise, all the methods added will be available for all controllers.
+
+class ApplicationController < ActionController::Base
+  helper :all # include all helpers, all the time
+
+  # See ActionController::RequestForgeryProtection for details
+  # Uncomment the :secret if you're not using the cookie session store
+  protect_from_forgery # :secret => 'c36eb9f87282c4f69ce511468e6446c5'
+end
