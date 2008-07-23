f817c718021c82d397d16f1b69c2fe2205fcb01b
Tom Howlett <tom@tomhowlett.com>
2008-07-23 21:57:15 +0100

    sorted restful controller

--- app/controllers/producers_controller.rb
+++ app/controllers/producers_controller.rb
@@ -10,19 +10,32 @@ class ProducersController < ApplicationController
   end
 
   def new
-
+    @producer = Producer.new
+    render :action => "edit"
   end
 
   def create
+    @producer = Producer.new(params[:producer])
+    @producer.image = Image.new(params[:image]) if(params[:image])
+    if(@producer.save)
+      flash[:notice] = 'Your details have been saved'
+      redirect_to(@producer)
+    else
+      render :action => "edit"
+    end
 
   end
 
   def update
     @producer = Producer.find(params[:id])
-    @producer.image = Image.new(params[:image])
-    @producer.update_attributes(params[:producer]) if(params[:producer])
-    flash[:notice] = "You've been updated"
-    redirect_to(@producer)
+    @producer.image = Image.new(params[:image]) if(params[:image][:uploaded_data].length > 0)
+
+    if(@producer.update_attributes(params[:producer]))
+      flash[:notice] = "You've been updated"
+      redirect_to(@producer)
+    else
+      render :action => "edit"
+    end
   end
 
   def edit
