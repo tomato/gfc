f0b7cb43db2a32f5b87779013e4e391053ad65c5
Tom Howlett <tom@tomhowlett.com>
2008-07-27 20:19:10 +0100

    added validation and test to producers

--- app/models/producer.rb
+++ app/models/producer.rb
@@ -1,10 +1,19 @@
 require 'image_helpers'
 
 class Producer < ActiveRecord::Base
-  has_many :producer_sections
-  has_one :image, :as => :section
   include ImageHelpers
   
+  has_many :producer_sections
+  has_one :image, :as => :section
+  
+  validates_presence_of :name, :summary, :email, :tel, :address
+  validates_uniqueness_of :name
+  validates_associated :producer_sections, :image
+  validates_format_of :email, :with => /[\w\-\.]+@[\w\-]+\.[a-z\.]{2,6}/
+  validates_format_of :website, :with => /www\.\w+\.[\w\.]{2,6}/
+  validates_length_of :summary, :in => 20..100
+  validates_length_of :name, :in => 3..50
+
   def Producer.get(num)
     Producer.find(:all, :limit => num)
   end
--- test/unit/producer_test.rb
+++ test/unit/producer_test.rb
@@ -21,4 +21,59 @@ class ProducerTest < ActiveSupport::TestCase
     assert_not_nil p.not_null_image
     assert_equal 'noimage.jpg', p.not_null_image, "i=" + p.not_null_image
   end
+
+  #Validation Tests
+  def test_first_fixture_valid
+    p = Producer.find(:first)
+    assert p.valid?, p.errors.each.to_s
+  end
+
+  def test_invalid_with_empty_attributes
+    p = Producer.new
+    assert !p.valid?
+    assert p.errors.invalid?(:name)
+    assert p.errors.invalid?(:summary)
+    assert p.errors.invalid?(:address)
+    assert p.errors.invalid?(:email)
+    assert p.errors.invalid?(:tel)
+  end
+  
+  def test_uniqueness_of_name
+    p1 = get_jimmy
+    p2 = Producer.new(:name => p1.name)
+    assert !p2.valid?
+    assert p2.errors.invalid?(:name)
+  end
+  
+  def test_dodgy_email_invalid
+    p = get_jimmy
+    p.email = "not a very good email"
+    assert !p.valid?
+    assert p.errors.invalid?(:email)
+  end
+
+  def test_dodgy_website_invalid
+    p = get_jimmy
+    p.website = "a a "
+    assert !p.valid?
+    assert p.errors.invalid?(:website)
+  end
+
+  def test_summary_to_short
+    p = get_jimmy
+    p.summary = "a"
+    assert !p.valid?
+    assert p.errors.invalid?(:summary)
+  end
+
+  private
+
+  def get_jimmy
+    p = Producer.find(:first)
+    assert p.valid?
+    p
+  end
+
+
+
 end
