93f96969dd1ca3e3e0968f5a4615378899b38ced
Tom Howlett <tom@tomhowlett.com>
2008-09-10 08:44:11 +0100

    tidied up css helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,12 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-  def c(i)
-    i+=1
-   (i%2)+1
+
+  def flip_flop(i)
+   "section#{(i%2)+1}"
   end
+
+  def grid(i)
+    "Cell#{i+1}"
+  end
+
 end
