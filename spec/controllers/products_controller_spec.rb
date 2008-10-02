f3c3dd1746c6008be57d2725f2f27f52e35a1d8a
Tom Howlett <tom@tomhowlett.com>
2008-10-02 08:50:49 +0100

    Added some tests to product controller to prove auth, added link to new on index but not working

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -1,30 +1,50 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 include AuthenticatedTestHelper
 
-describe ProductsController do
+describe ProductsController, "with new" do
 
   before do
-    @producer = mock_model(Producer)
-    @product = mock_model(Product)
-    @producer.stub!(:products).and_return([])
-    @producer.stub!(:save).and_return(true)
-    Producer.stub!(:find).and_return(@producer)
-    @producer.stub!(:user).and_return stub_login_as('admin')
+    create_mocks
   end
 
-  it "should respond to new" do
+  it "should respond to new for admin" do
     get :new, :producer_id => 1
     response.should render_template :edit
   end
 
-#  it "should create new product on new" do
-#    Product.should_receive(:new).and_return(@product)
-#    get :new
-#  end
+  it "should respond to new for producer" do
+    @producer.stub!(:name).and_return('Fred')
+    @producer.stub!(:user).and_return stub_login_as('Fred')
+    get :new, :producer_id => 1
+    response.should render_template :edit
+  end
+
+  it "should respond with redirect for plonky" do
+    @producer.stub!(:user).and_return 'Fred'
+    stub_login_as('plonky')
+    get :new, :producer_id => 1
+    response.should be_redirect
+  end
+
+  it "should create new product on new" do
+    Product.should_receive(:new).and_return(@product)
+    get :new, :producer_id => 1
+  end
 
 #  it "should render show after create" do
 #    post :create
 #    response.should render_template :show
 #  end
 
+end
+
+private 
+
+def create_mocks
+    @producer = mock_model(Producer)
+    @product = mock_model(Product)
+    @producer.stub!(:products).and_return([])
+    @producer.stub!(:save).and_return(true)
+    Producer.stub!(:find).and_return(@producer)
+    @producer.stub!(:user).and_return stub_login_as('admin')
 end
