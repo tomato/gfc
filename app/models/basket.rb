c67760820ee2c3c83de8242b822e7fd019a9228d
Tom Howlett <tom@tomhowlett.com>
2008-12-09 19:18:03 +0000

    Working basket but could do with a refactor

--- app/models/basket.rb
+++ app/models/basket.rb
@@ -6,13 +6,39 @@ class Basket
     @items = {}
   end
 
-  def count
-    @items.count
+  def total_quantity
+    total_quantity = 0
+    items.each{|key,value| total_quantity += value}
+    total_quantity
   end
 
   def add(product, quantity=1)
     if(Product.exists?(product))
-      @items[product.id] = quantity
+      if(@items[product.id])
+        @items[product.id] += quantity
+      else
+        @items[product.id] = quantity
+      end
+      if(@items[product.id] < 0)
+       @items[product.id] -= quantity
+       raise ArgumentError, "Quantity cannot be negative", caller 
+      end
+      if(@items[product.id] == 0)
+        @items.delete product.id
+      end
     end
   end
+
+  def remove(product, quantity=1)
+    add(product, quantity*-1)
+  end
+
+  def update_quantity(product, quantity)
+    if(@items.has_key?(product.id) && quantity > 0)
+      @items[product.id] = quantity
+    else
+      raise ArgumentException, "Product Not Found or quantity negative", caller
+    end
+  end
+
 end
--- spec/models/basket_spec.rb
+++ spec/models/basket_spec.rb
@@ -4,12 +4,59 @@ require File.expand_path(File.dirname(__FILE__) + '/../factories')
 describe Basket do
   before(:each) do
     @basket = Basket.new
+    @p = Factory(:product)
   end
 
   it "should accept a valid product" do
-    @basket.add Factory(:product)
-    @basket.count.should eql(1)
+    @basket.add @p
+    @basket.total_quantity.should eql(1)
   end
 
+  it "should not add a non persisted product" do
+    @basket.add Product.new
+    @basket.total_quantity.should eql(0)
+  end
 
+  it "should add a specific quantity to a basket" do
+    @basket.add @p, 2
+    @basket.total_quantity.should eql(2)
+  end
+
+  it "should remove a specifc quantity from a basket item" do
+    @basket.add @p, 3
+    @basket.remove @p, 1
+    @basket.total_quantity.should eql(2)
+  end
+
+  it "should throw exception if quantity of an item is negative" do
+    lambda{ @basket.remove(@p, 1) }.should raise_error(ArgumentError)
+    @basket.total_quantity.should eql(0)
+  end
+
+  it "should amend quantity where the item already exists" do
+    @basket.add @p ,1
+    @basket.add @p ,2
+    @basket.total_quantity.should eql(3)
+  end
+
+  it "should update the quantity of a product" do
+    @basket.add @p
+    @basket.update_quantity @p, 2
+    @basket.total_quantity.should eql(2)
+  end
+
+  it "should raise error it quantity is updated of product in the basket" do
+    lambda{ @basket.update_quantity @p, 2}.should raise_error
+  end
+
+  it "should raise error if quantity is set to negative when updating quantity" do
+    @basket.add @p
+    lambda{ @basket.update_quantity @p, -1}.should raise_error
+  end
+
+  it "should remove product if quantity is 0" do
+    @basket.add @p
+    @basket.remove @p
+    @basket.items.should_not have_key @p.id
+  end
 end
